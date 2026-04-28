528b9f3214a81cc06421a6e2a70bf87a50e6266c
gustamiranda00 <gustavomiranda@colegiopaulodetarso.com.br>
2026-04-28 14:47:46 +0000

    git add tarefas concluidas

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
-const taskBtn = document.querySelector("#taskBtn")
-const taskList = document.querySelector("#taskList")
-const taskInput = document.querySelector("#taskInput")
-const clearAllBtn = document.querySelector("#clearAllBtn")
-const taskCounter = document.querySelector("#taskCounter")
-
 function updateCounter(){
     const total = document.querySelectorAll("#taskList li").length;
-    taskCounter.textContent = `${total} tarefas`;
+    const completed = document.querySelectorAll(".completed").length;
+
+    taskCounter.textContent = `${completed} de ${total} tarefas concluídas`;
 }
-
-function addTask(){
-    const text = taskInput.value.trim();
-    if(text === '') return;
-
-    const li = document.createElement("li");
-    li.textContent = text;
-
-    taskList.appendChild(li);
-
-    taskInput.value = '';
-    updateCounter();
-}
-
-taskBtn.addEventListener("click", addTask)
-
-taskInput.addEventListener("keypress", function(e){
-    if(e.key === "Enter"){
-        addTask();
-    }
-})
-
-clearAllBtn.addEventListener("click", function(){
-    taskList.innerHTML = '';
-    updateCounter();
-})
